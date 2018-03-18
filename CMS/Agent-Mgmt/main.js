0d2601020cffe3cd83d077c94a081bffb38431ff
richTheCreator <moralesrichard@live.com>
2018-03-18 12:14:24 -0500

    USA phone number formatting.

--- CMS/Agent-Mgmt/main.js
+++ CMS/Agent-Mgmt/main.js
@@ -1,10 +1,57 @@
 $(document).ready(function() {
-  console.log('Loaded outside dep via rawgit!')
+  console.log('-----> Loaded outside dep via rawgit!')
 
-  // $('[data-label="R-Nav Page"]').css('left', '1277px')
-  // $('[data-label="DP-Live-Agent"],[data-label="DP-Live-Header"], [data-label="DP-Save-Rnav"]').css('left', '1281px')
+
+// USA PHONE NUMBER FORMATTING
+  // list of axure cell inputs
+  var cell1 = '[data-label="TF-Form-Cell" input]';
+
+  // Format the phone number as the user types it
+  cell1.addEventListener('keyup', function(evt) {
+    var phoneNumber = cell1;
+    var charCode = (evt.which)
+      ? evt.which
+      : evt.keyCode;
+    phoneNumber.value = phoneFormat(phoneNumber.value);
+  });
+
+  // We need to manually format the phone number on page load
+  cell1.value = phoneFormat(cell1);
+
+  // A function to determine if the pressed key is an integer
+  function numberPressed(evt) {
+    var charCode = (evt.which)
+      ? evt.which
+      : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && (charCode < 36 || charCode > 40)) {
+      return false;
+    }
+    return true;
+  }
+
+  // A function to format text to look like a phone number
+  function phoneFormat(input) {
+    // Strip all characters from the input except digits
+    input = input.replace(/\D/g, '');
+
+    // Trim the remaining input to ten characters, to preserve phone number format
+    input = input.substring(0, 10);
+
+    // Based upon the length of the string, we add formatting as necessary
+    var size = input.length;
+    if (size == 0) {
+      input = input;
+    } else if (size < 4) {
+      input = '(' + input;
+    } else if (size < 7) {
+      input = '(' + input.substring(0, 3) + ') ' + input.substring(3, 6);
+    } else {
+      input = '(' + input.substring(0, 3) + ') ' + input.substring(3, 6) + ' - ' + input.substring(6, 10);
+    }
+    return input;
+  }
+
 })
-
 
 // Below is the function that gets placed into axures onPageLoad event
 // Place this on every page in axure you need it to affect.
@@ -21,3 +68,7 @@ $(document).ready(function() {
 //   s.src = 'https://rawgit.com/richTheCreator/axureBridge/master/CMS/Agent-Mgmt/main.js';
 //   $("head").append(s, link);
 // }()
+
+// How to target data attrs,=
+// $('[data-label="R-Nav Page"]').css('left', '1277px')
+// $('[data-label="DP-Live-Agent"],[data-label="DP-Live-Header"], [data-label="DP-Save-Rnav"]').css('left', '1281px')
