d9304775f875765076666f0561ef265a843c7233
richTheCreator <moralesrichard@live.com>
2018-03-17 19:47:11 -0500

    Init commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var sentiment = require('sentiment');
+
+var userText = "Easy to use. A much more elegant method to creating the profile versus doing it through say Sharepoint."
+
+var r1 = sentiment(userText)
+
+console.log(r1);
+
+
+javascript:void($('[data-label="DP-Live_agent"]').css('left','1280px'));
+javascript:void($('[data-label="R-Nav Page"]').css('left','1280px'));
