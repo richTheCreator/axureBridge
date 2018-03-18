c9f474e5bbb220a5454bf5700db2226e7aa470be
richTheCreator <moralesrichard@live.com>
2018-03-17 23:02:32 -0500

    CSS source file added.

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-// javascript:void($('[data-label="DP-Live_agent"]').css('left','1280px'));
-
 $(document).ready(function() {
-  console.log('LOADED!')
-  $('[data-label="R-Nav Page"]').css('left', '1277px')
-  $('[data-label="DP-Live-Agent"],[data-label="DP-Live-Header"], [data-label="DP-Save-Rnav"]').css('left', '1281px')
+  console.log('Loaded outside dep via rawgit!')
+
+  // $('[data-label="R-Nav Page"]').css('left', '1277px')
+  // $('[data-label="DP-Live-Agent"],[data-label="DP-Live-Header"], [data-label="DP-Save-Rnav"]').css('left', '1281px')
 })
-// javascript:void();
