32bc2f9086c90dcd01cecc4cafe93e720ed334b9
richTheCreator <moralesrichard@live.com>
2018-03-17 20:14:32 -0500

    Targeting CSS via jquery.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // javascript:void($('[data-label="DP-Live_agent"]').css('left','1280px'));
 
-$( document ).ready(function(){
+$(document).ready(function() {
   console.log('LOADED!')
+  $('[data-label="R-Nav Page"], [data-label="DP-Live_agent"]').css('left','1280px')
 })
-// javascript:void($('[data-label="R-Nav Page"]').css('left','1280px'));
+// javascript:void();
