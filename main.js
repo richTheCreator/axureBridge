8a58561f22844ab2eece4f09fc0f5b37962192ec
richTheCreator <moralesrichard@live.com>
2018-03-17 20:35:12 -0500

    Fixing RNav placement.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function() {
   console.log('LOADED!')
-  $('[data-label="R-Nav Page"], [data-label="DP-Live_agent"]').css('left','1280px')
+  $('[data-label="R-Nav Page"]').css('left', '1277px')
+  $('[data-label="DP-Live-Agent"],[data-label="DP-Live-Header"], [data-label="DP-Save-Rnav"]').css('left', '1281px')
 })
 // javascript:void();
