d07eca7bf334738de6df67c742c6d8edebb9e926
richTheCreator <moralesrichard@live.com>
2018-03-19 22:03:35 -0500

    Adding common helper fns that can be reused.

--- Common/main.js
+++ Common/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-  console.log('-----> Agent-Mngmt dep loaded via rawgit!')
+  console.log('-----> Common dep loaded via rawgit!')
 
   // ----- USA PHONE NUMBER FORMATTING -----
 
   // list of axure phone number inputs
-  var cellF = '[data-label="TF-Form-Cell"] input';
-  var cellL = '[data-label="TF-Live-Cell"] input';
-  var faxF = '[data-label="TF-Form-Fax"] input';
-  var faxL = '[data-label="TF-Live-Fax"] input';
-  var officeF = '[data-label="TF-Form-Office"] input';
-  var officeL = '[data-label="TF-Live-Office"] input';
+  var pf1 = '[data-label="Phone-Format1"] input';
+  var pf2 = '[data-label="Phone-Format2"] input';
+  var pf3 = '[data-label="Phone-Format3"] input';
+  var pf4 = '[data-label="Phone-Format4"] input';
+  var pf5 = '[data-label="Phone-Format5"] input';
+  var pf6 = '[data-label="Phone-Format6"] input';
 
   var current;
   // Format the phone number as the user types it
@@ -20,24 +20,25 @@ $(document).ready(function() {
 
     //determine which input active
     switch (parent) {
-      case 'TF-Form-Cell':
-        current = cellF;
+      case 'Phone-Format1':
+        current = pf1;
         break;
-      case 'TF-Live-Cell':
-        current = cellL;
+      case 'Phone-Format2':
+        current = pf2;
         break;
-      case 'TF-Form-Fax':
-        current = faxF;
+      case 'Phone-Format3':
+        current = pf3;
         break;
-      case 'TF-Live-Fax':
-        current = faxL;
+      case 'Phone-Format4':
+        current = pf4;
         break;
-      case 'TF-Form-Office':
-        current = officeF;
+      case 'Phone-Format5':
+        current = pf5;
         break;
-      case 'TF-Live-Office':
-        current = officeL;
+      case 'Phone-Format6':
+        current = pf6;
         break;
+
     }
     var phoneNumber = $(current);
     var formatted = phoneFormat($(current).val());
@@ -83,24 +84,33 @@ $(document).ready(function() {
     return input;
   }
 
+  // ----- DISABLE SCROLLING IOS MODELS -----
+  var bodyScrollTop;
+
+  setBodyUnscrollable(value) {
+    if (value)
+      bodyScrollTop = document.body.scrollTop;
+    document.body.style.overflow = (value)
+      ? 'hidden'
+      : '';
+    document.body.style.position = (value)
+      ? 'fixed'
+      : '';
+    document.body.style.left = (value)
+      ? '0'
+      : '';
+    document.body.style.right = (value)
+      ? '0'
+      : '';
+    document.body.style.top = (value)
+      ? -bodyScrollTop + 'px'
+      : '';
+    if (!value)
+      document.body.scrollTop = bodyScrollTop;
+    }
+
+  $('[data-label="iOS-NoScroll"] div').click(function() {
+    alert('FIRED!')
+  });
+
 })
-
-// Below is the function that gets placed into axures onPageLoad event
-// Place this on every page in axure you need it to affect.
-
-// javascript: void function load() {
-//
-//   var link = document.createElement('link');
-//   link.href = 'https://rawgit.com/richTheCreator/axureBridge/master/CMS/Agent-Mgmt/main.css';
-//   link.rel = 'stylesheet';
-//   link.type = 'text/css';
-//
-//   var s = document.createElement('script');
-//   s.type = 'text/javascript';
-//   s.src = 'https://rawgit.com/richTheCreator/axureBridge/master/CMS/Agent-Mgmt/main.js';
-//   $("head").append(s, link);
-// }()
-
-// How to target data attrs,=
-// $('[data-label="R-Nav Page"]').css('left', '1277px')
-// $('[data-label="DP-Live-Agent"],[data-label="DP-Live-Header"], [data-label="DP-Save-Rnav"]').css('left', '1281px')
